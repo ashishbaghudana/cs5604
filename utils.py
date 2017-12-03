61b910e5249ad87ba1b9bda92713a4d56bff5a33
Ashish Baghudana <ashish.baghudana26@gmail.com>
2017-12-03 10:19:03 -0500

    Set progress bar for preprocessing

--- utils.py
+++ utils.py
@@ -15,17 +15,15 @@ class WebpageTokensReader(object):
 
     def preprocess(self):
         logging.info('Start preprocessing all documents')
-        bar = progressbar.ProgressBar(maxval=self.num_docs)
+        bar = progressbar.ProgressBar(max_value=self.num_docs)
         with open(self.filename, 'r') as freader:
             if self.pipeline is None:
-                for line in freader:
+                for line in bar(freader):
                     self.documents[line.split('\t')[0].strip()] = line.split('\t')[1].strip().split(',')
-                    bar.update(bar.currval + 1)
             else:
                 logging.info('Using the preprocessing pipeline to process each document')
-                for line in freader:
+                for line in bar(freader):
                     self.documents[line.split('\t')[0].strip()] = self.pipeline.preprocess(line.split('\t')[1].strip())
-                    bar.update(bar.currval + 1)
 
     def __iter__(self):
         for document in self.documents.values():
